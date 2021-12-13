8229de8dbfcacb34e9e58f2e3dd87d6598041281
Zainoob <i190654@nu.edu.pk>
2021-12-13 17:36:09 +0500

    First commit xD

---
+++ src/application/Clinic_Controller.java
@@ -0,0 +1,292 @@
+package application;
+
+import java.io.IOException;
+
+//import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class Clinic_Controller {
+
+	@FXML
+	Button login;
+
+	@FXML
+	Button Re_login;
+	
+	@FXML
+	Button Home;
+	
+	@FXML
+	TextArea feedback;
+	
+	@FXML
+	Button Pay;
+
+	@FXML
+	Button Submit;
+	
+/*	@FXML
+	TextField []  tf ;
+*/	
+	@FXML
+	TextField  user_n = new TextField();
+	
+	@FXML
+	TextField pass_w = new TextField();
+	
+	@FXML
+	TextField  appointment_id = new TextField();
+	
+	@FXML
+	TextField patient_id = new TextField();
+	
+	
+/*	@FXML
+	TextField InvDisp= new TextField();
+*/
+	String str1 = "a";
+	String str2 = "a";
+	String str3 = "1";
+	String str4 = "2";
+	
+    @FXML
+    private void LogIn() throws IOException {
+      
+    	String username = user_n.getText();
+        String password = pass_w.getText();
+        if (username.equals(str1) && password.equals(str2)) {
+    		Stage stage = (Stage) login.getScene().getWindow();
+   	     
+    	    stage.close();
+    	    
+    	     Scene quizScene = new Scene(FXMLLoader.load(getClass().
+    	     getResource("Main_Menu.fxml")));
+    		 Stage primaryStage1 = new Stage();
+    		 primaryStage1.setScene(quizScene);
+    		 primaryStage1.show();
+        }
+        
+        else {
+    		Stage stage = (Stage) login.getScene().getWindow();
+      	     
+    	    stage.close();
+
+    	     Scene quizScene = new Scene(FXMLLoader.load(getClass().
+    	     getResource("Re_Login.fxml")));
+    		 Stage primaryStage1 = new Stage();
+    		 primaryStage1.setScene(quizScene);
+    		 primaryStage1.show();
+        }
+        
+    }
+    
+    @FXML
+    private void Book_appointment(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("Book_Appointment.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+   
+    @FXML
+    private void Cancel_appointment(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("Cancel_Appointment.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    @FXML
+    private void view_appointment(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("View_Appointment.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    @FXML
+    private void Reschedule_appointment(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("Reschedule_Appointment.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+
+    
+    @FXML
+    private void  Appointmentoptions(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("AppointmentOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    
+    @FXML
+    private void  PrescriptionOptions(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("PrescriptionOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+   
+    @FXML
+    private void  Prescribe_patient(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("PrescriptionOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    @FXML
+    private void  Edit_prescription(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("PrescriptionOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+   
+    @FXML
+    private void  View_prescription(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("PrescriptionOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    
+    @FXML
+    private void  ProfileOptions(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("ProfileOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    @FXML
+    private void  ViewStaffProfiles(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("ProfileOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    @FXML
+    private void  ViewPatientProfile(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("ProfileOptions.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    @FXML
+    private void  Give_Feedback(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("Give_Feedback.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    
+    @FXML
+    private void Enter_Feedback(ActionEvent event) throws IOException {
+    	String feedbackk=feedback.getText();
+    	System.out.print(feedbackk);
+    }
+    @FXML
+    private void Cancelled() throws IOException {
+    	String p_id = patient_id.getText();
+        String app_id = appointment_id.getText();
+        if (p_id.equals(str3) && app_id.equals(str4)) {
+			Stage stage = (Stage) Submit.getScene().getWindow();
+	 	     
+		    stage.close();
+	
+		     Scene quizScene = new Scene(FXMLLoader.load(getClass().
+		     getResource("Cancellation.fxml")));
+			 Stage primaryStage1 = new Stage();
+			 primaryStage1.setScene(quizScene);
+			 primaryStage1.show();
+        }
+        
+        else {
+			Stage stage = (Stage) Submit.getScene().getWindow();
+	 	     
+		    stage.close();
+	
+		     Scene quizScene = new Scene(FXMLLoader.load(getClass().
+		     getResource("Re_Cancel.fxml")));
+			 Stage primaryStage1 = new Stage();
+			 primaryStage1.setScene(quizScene);
+			 primaryStage1.show();
+        }
+    }
+    
+
+    
+    
+    @FXML
+    private void Make_Payment(ActionEvent event) throws IOException {
+    	Parent CalenderView = FXMLLoader.load(getClass().getResource("Make_payment.fxml"));
+		Scene CalenderScene=  new Scene(CalenderView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(CalenderScene);
+		window.setTitle("");
+		window.show();
+    }
+    
+    @FXML
+    private void home(ActionEvent event) throws IOException {
+    	Parent HomeView = FXMLLoader.load(getClass().getResource("Main_Menu.fxml"));
+		Scene HomeScene=  new Scene(HomeView);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(HomeScene);
+		window.setTitle("Main Menu");
+		window.show();
+    }
+    
+    @FXML
+    private void Payment() throws IOException {
+		Stage stage = (Stage) Pay.getScene().getWindow();
+ 	     
+	    stage.close();
+
+	     Scene quizScene = new Scene(FXMLLoader.load(getClass().
+	     getResource("Payment_done.fxml")));
+		 Stage primaryStage1 = new Stage();
+		 primaryStage1.setScene(quizScene);
+		 primaryStage1.show();
+    }
+    
+
+
+}
---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+
+
+public class Main extends Application
+{
+
+	@Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setTitle("MEDICSi Health Care");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
